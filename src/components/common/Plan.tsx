0a22585ee9f59b7810cf225f2e01c810e72d5f8d
Varun Verma <varun7086verma@gmail.com>
2024-12-14 14:01:38 +0530

    adding the changes to the plan section

--- src/components/common/Plan.tsx
+++ src/components/common/Plan.tsx
@@ -1,32 +1,35 @@
-import icons from '../../assets/icons.svg';
+import React from 'react';
 
-const Plan = ({billingPlanYearly, planImg, planName, planPrice, onInputChange ,formPlan}: {
-    planImg: string,
-    planName: string,
-    onInputChange: React.FormEventHandler,
-    planPrice: number,
-    billingPlanYearly: boolean,
-    formPlan : 'string'
-}) => {
-    return (
-        <>
-            <li className="form__plans--plan">
-                <label >
-                    <div className="form__plan--icon">
-                        <svg className='icon'>
-                            <use href={icons + planImg}></use>
-                        </svg>
-                    </div>
-                    <span className='form__plan--name'> {planName}</span>
-                    <input
-                        checked={planName.toLowerCase() === formPlan}
-                        value={planName.toLowerCase()}
-                        onChange={onInputChange} type="radio" name="formPlan" />
-                    <p className="form__plan--price">{billingPlanYearly ? `$${planPrice*12}/yr` : `$${planPrice}/mo`}</p>
-                </label>
-            </li>
-        </>
-    )
+interface PlanProps {
+  planImg: string;
+  planName: string;
+  planPrice: number;
+  onInputChange: React.ChangeEventHandler<HTMLInputElement>;
+  formPlan: string;
+  billingPlanYearly: boolean;
 }
+
+const Plan: React.FC<PlanProps> = ({ planImg, planName, planPrice, onInputChange, formPlan, billingPlanYearly }) => {
+  return (
+    <li className="form__plans--plan">
+      <label>
+        <div className="form__plan--icon">
+          <img src={planImg} alt={planName} />
+        </div>
+        <span className="form__plan--name">{planName}</span>
+        <input
+          type="radio"
+          name="formPlan"
+          value={planName}
+          checked={formPlan === planName}
+          onChange={onInputChange}
+        />
+        <span className="form__plan--price">
+          {billingPlanYearly ? `+$${planPrice * 12}/yr` : `+$${planPrice}/mo`}
+        </span>
+      </label>
+    </li>
+  );
+};
 
 export default Plan;
